93b7116d12724c982d4adde4f0d150f1ecb3df71
Gennady Evstratov <g.evstratov@gmail.com>
2026-02-22 00:46:52 +0300

    Refactor gog-email skill: remove shell scripts, use inline commands

--- workspace/skills/gog-email/email-send.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Send email
-set -e
-
-HOST="bezalel"
-PASSWORD="my-secret-password"
-
-# Pass all arguments directly to gog send
-ssh "$HOST" "GOG_KEYRING_PASSWORD=$PASSWORD gog gmail send $*"
--- workspace/skills/gog-email/email-unread.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# List unread emails
-set -e
-
-HOST="bezalel"
-PASSWORD="my-secret-password"
-
-ssh "$HOST" "GOG_KEYRING_PASSWORD=$PASSWORD gog gmail messages search is:unread -j --results-only --max=20"
